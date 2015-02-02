de7ae94a9cc027b0fb11890ae49036f88c202cc7
Eric Robinson <eric.z.robinson@gmail.com>
2015-02-01 19:09:34 -0500

    added 2nd project
    Bulk Folder Permissions

---
+++ BulkFolderPermissions/BulkRemoveUserPermissions/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace BulkRemoveUserPermissions
+{
+    class Program
+    {
+        public static DirectoryInfo[] folders;
+
+        static void Main(string[] args)
+        {
+            string folder;
+            string user;
+            
+            do { Console.WriteLine("Please enter a directory to remove permissions from sub-folders."); folder = Console.ReadLine(); } while (folder == "");
+            GetSubdirectories(folder);
+            RemoveInheritence();
+            RemoveAccount();
+            //do { Console.WriteLine("Pease enter user or group you want removed from these folders"); user = Console.ReadLine(); } while (folder == "");
+
+
+        }
+
+        static void RemoveAccount()
+        {
+            foreach (DirectoryInfo folder in folders)
+            {
+                var accountToRemove = "BUILTIN\\Users";
+                var security = folder.GetAccessControl();
+                var rules = security.GetAccessRules(true, true, typeof(NTAccount));
+
+                foreach (FileSystemAccessRule rule in rules)
+                {
+                    if (rule.IdentityReference.Value == accountToRemove)
+                    {
+                        security.RemoveAccessRuleSpecific(rule);
+                        folder.SetAccessControl(security);
+                    }
+
+                }
+            }
+        }
+
+        static void RemoveInheritence()
+        {
+            //below command turns off inheritence and copies inherited aces to the object.
+            foreach (DirectoryInfo folder in folders)
+            {
+              
+            DirectorySecurity dSec = folder.GetAccessControl();
+                dSec.SetAccessRuleProtection(true, true);
+                folder.SetAccessControl(dSec);
+            }
+        }
+
+        static void GetSubdirectories(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            folders = directory.GetDirectories();
+
+            foreach (DirectoryInfo folder in folders)
+                Console.WriteLine(folder.Name);
+        }
+    }
+}
---
+++ BulkFolderPermissions/README.md
@@ -0,0 +1,16 @@
+BulkFolderPermissions
+=====================
+
+I was tasked to remove a inheritance and remove BUILTIN\Users from a bunch of folders. This was the program I created to handle that. 
+
+How-to
+======
+- Download the zipped file
+- Start the program located [../BulkFolderPermissions/BulkRemoveUserPermissions/bin/Debug/BulkRemoveUserPermissions.exe](https://github.com/robinsone/BulkFolderPermissions/tree/master/BulkRemoveUserPermissions/bin/Debug)
+- Make sure the program has administrative rights
+- when the program prompts you, type the full path to the folder.
+- Profit! 
+
+## More information
+Currently this program will remove inheritence and builtin\users on the sub-folders and files.
+
